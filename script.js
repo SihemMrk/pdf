0adadba1f88fea0d13d0123f7b5a792bb5c67b98
Sihem Meriki <sihemmeriki2@gmail.com>
2020-01-20 15:20:58 +0100

    require not working

---
+++ script.js
@@ -0,0 +1,20 @@
+const { HummusRecipe } = require("hummus-recipe");
+console.log(HummusRecipe);
+// const pdfDoc = new HummusRecipe(
+//   "CVSihemMeriki-Dev1.pdf",
+//   "CVSihemMeriki-Dev1.pdf"
+// );
+// const pdfDoc = new HummusRecipe("new", "output.pdf", {
+//   version: 1.6,
+//   author: "John Doe",
+//   title: "Hummus Recipe",
+//   subject: "A brand new PDF"
+// });
+
+// pdfDoc
+//   .createPage("letter-size")
+//   .endPage()
+//   .endPDF();
+
+// // document.getElementById("pdf").innerHTML = pdfDoc;
+// console.log(pdfDoc);
